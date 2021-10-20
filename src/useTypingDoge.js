4ef09c7379989a81b77b204202c7bf8d4a022439
Arman Ali <armanalicid@gmail.com>
2021-10-20 20:34:28 +0530

    increased timing for testing

--- src/useTypingDoge.js
+++ src/useTypingDoge.js
@@ -1,7 +1,7 @@
 import {useState,useEffect, useRef} from "react"
 
 function useTypingDoge(){
-    const STARTING_TIME = 10
+    const STARTING_TIME = 30
     
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
