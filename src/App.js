a61edd6e6f1f77d9a59b3273523a8839b4f642fc
Arman Ali <armanalicid@gmail.com>
2021-10-20 14:12:13 +0530

    TypingDoge V1.0 done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
+import useTypingDoge from "./useTypingDoge"
 
 function App() {
+  const [textBoxRef, handleChange,text,timeRemaining,startGame,isTimeRunning,wordCount] = useTypingDoge()  
   return (
     <div className="App">
-      <header className="App-header">
-       Welcome to Typing Dog
-      </header>
-    </div>
+      <h1 className="h1 text-light mb-5">How fast can you type?</h1>
+      <h4 className="h4">Time remaining: {timeRemaining}</h4>
+      <div id="textContainer">
+            <textarea
+                ref={textBoxRef}
+                onChange={handleChange}
+                value={text}
+                disabled={!isTimeRunning}
+            />
+            </div>
+            
+            <button className="btn btn-success btn-lg"
+                onClick={startGame}
+                disabled={isTimeRunning}
+            >Start
+            </button>
+            <h1 className="h1">Typing Speed: {wordCount} wpm.</h1>
+            </div>
   );
 }
 
