cff4dc7cf0d98ded3dbaed49fd18584f9d4def95
Arman Ali <armanalicid@gmail.com>
2021-10-21 10:42:53 +0530

    increased duration

--- src/useTypingDoge.js
+++ src/useTypingDoge.js
@@ -1,7 +1,7 @@
 import {useState,useEffect, useRef} from "react"
 
 function useTypingDoge(){
-    const STARTING_TIME = 30
+    const STARTING_TIME = 60
     
     const [text, setText] = useState("")
     const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
